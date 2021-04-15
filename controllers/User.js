be3a4b790b3d0a58c1c65dee6985cee45a7db832
babalolajnr <babalolajnr@gmail.com>
2021-04-15 21:21:41 +0100

    added joi use schema

--- controllers/User.js
+++ controllers/User.js
@@ -1,13 +1,30 @@
 const User = require('../models/User')
+const Joi = require('joi')
 
 const UserController = {
     createUser: function (req, res) {
 
-        User.create({ ...req.body }).then(() => {
-            res.send('User created')
-        }).catch((err) => {
-            res.send(err)
+        // Joi validation schema
+        const schema = Joi.object({
+            firstName: Joi.string().max(20).required(),
+            lastName: Joi.string().max(20).required(),
+            email: Joi.string().email().required(),
+            phone: Joi.string().max(15),
+            password: Joi.string().required(),
+            passwordConfirmation: Joi.ref('password')
         })
+
+        const { error, value } = schema.validate({ ...req.body })
+
+        if (error) {
+            res.send(error)
+        } else {
+            User.create({ ...req.body }).then(() => {
+                res.send('User created')
+            }).catch((err) => {
+                res.send(err)
+            })
+        }
     }
 }
 
