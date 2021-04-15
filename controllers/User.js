bf915943a4112e0a53aa6ac9f8bb9db350977dec
babalolajnr <babalolajnr@gmail.com>
2021-04-15 17:50:52 +0100

    added promise callbacks

--- controllers/User.js
+++ controllers/User.js
@@ -2,8 +2,12 @@ const User = require('../models/User')
 
 const UserController = {
     createUser: function (req, res) {
-        User.create({ ...req.body })
-        res.send('User created')
+
+        User.create({ ...req.body }).then(() => {
+            res.send('User created')
+        }).catch((err) => {
+            res.send(err)
+        })
     }
 }
 
