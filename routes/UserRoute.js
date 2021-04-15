4a51553245489e4bc332c2bfbfe5ed4664c78174
babalolajnr <babalolajnr@gmail.com>
2021-04-15 16:49:00 +0100

    added addUser route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const UserController = require('../controllers/User')
+const User = require('../models/User')
 
 router.get('/', function (req, res) {
     res.send('SignUp page')
 })
+
+router.post('/addUser', UserController.createUser)
 
 module.exports = router
