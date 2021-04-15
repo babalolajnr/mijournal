aa09e18144ad81537ed5c6605317032ae0ef9c21
babalolajnr <babalolajnr@gmail.com>
2021-04-15 17:50:07 +0100

    refactoring

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const port = 3000
 const UserRouter = require('./routes/UserRoute')
 const mongoose = require('mongoose');
 
+const moongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}
+
 //mongoose connection
-mongoose.connect('mongodb://localhost:27017/mijournal', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/mijournal', moongooseOptions)
+    .then(() => {
+        console.log('Database connection established')
+    }).catch((err) => {
+        console.log('Error connecting to the database')
+    })
 
 app.listen(port, () => {
-    console.log(`Listening on ${port}`)
+    console.log(`Listening on ${port} ...`)
 })
 
 app.use(express.json())
