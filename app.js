9a7b2830e0f7df8cddcef6a9ae27a1830c02aefc
babalolajnr <babalolajnr@gmail.com>
2021-04-15 16:47:03 +0100

    added mongoose connection

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const port = 3000
 const UserRouter = require('./routes/UserRoute')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mijournal', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/', UserRouter)
 app.listen(port, () => {
