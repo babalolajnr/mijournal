631ad6f47430841396701b740112641be6a13603
babalolajnr <babalolajnr@gmail.com>
2021-04-17 11:09:23 +0100

    removed user field from journal schema

--- models/Journal.js
+++ models/Journal.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
-const User = require('./User')
 const { Schema } = mongoose
 
-const journalSchema = new Schema ({
+const journalSchema = new Schema({
     entry: {
         required: true,
         type: String
     },
-    date: Date,
     favourite: Boolean,
-    user: User
+}, {
+    timestamps: true
 })
 
 module.exports = journalSchema
