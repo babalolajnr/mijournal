8814f13b271cfdd10d544e215cb0332629ba1399
babalolajnr <babalolajnr@gmail.com>
2021-04-19 11:44:33 +0100

    added passport authentication

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config()
+}
 const express = require("express")
 const mongoose = require('mongoose')
 const port = 3000
 const app = express()
 const helmet = require('helmet') // Helmet helps to secure Express apps by setting various HTTP headers.
-const UserRouter = require('./routes/UserRoute')
+const passport = require('passport')
+const initializePassport = require('./config/passport')
+const User = require("./models/User")
+const flash = require('express-flash')
+const session = require('express-session')
 const JournalRouter = require('./routes/JournalRoute')
+const UserRouter = require('./routes/UserRoute')
+
+initializePassport(passport)
 
 const moongooseOptions = {
     useNewUrlParser: true,
@@ -27,6 +37,26 @@ app.listen(port, () => {
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
+// Routes
+app.post('/login', passport.authenticate('local', {
+    failureFlash: true,
+    failWithError: true,
+    failureMessage: true,
+    successFlash: 'Welcome',
+    successMessage: 'Welcome',
+    successRedirect: '/'
+}))
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
 app.use('/user', UserRouter)
 app.use('/journal', JournalRouter)
