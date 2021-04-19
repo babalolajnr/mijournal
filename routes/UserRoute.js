c786f5d675165490801a0174a7cb93210c4fa04a
babalolajnr <babalolajnr@gmail.com>
2021-04-19 14:31:51 +0100

    added /hi route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const UserController = require('../controllers/User')
 
 router.post('/register', UserController.register)
+router.get('/hi', (req, res) => {
+    console.log(req.user)
+})
 
 module.exports = router
