725ab5ac817aeefb37c85050fc576ff109a70dfd
babalolajnr <babalolajnr@gmail.com>
2021-04-25 18:38:16 +0100

    removed register route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const UserController = require('../controllers/User')
 
-router.post('/register', UserController.register)
 router.get('/hi', (req, res) => {
     console.log(req.user)
 })
